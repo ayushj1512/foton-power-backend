7bd1be5b5aa9f0dbdb4c81c187bfd187e38dca11
Ayush Juneja <miray.ayushjuneja@gmail.com>
2026-06-06 18:11:01 +0530

    server.js changes for cors

--- server.js
+++ server.js
@@ -41,8 +41,10 @@ app.use(express.static(path.join(__dirname, "public")));
 const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:3001",
+  "http://localhost:3002",
   "http://127.0.0.1:3000",
   "http://127.0.0.1:3001",
+  "http://127.0.0.1:3002",
 
   // Production domains
   "https://fotonpower.in",
